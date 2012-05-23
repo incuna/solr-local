c6338fc3f248c104786ffa3a2ab8380435a32712
George Hickman <george@ghickman.co.uk>
2012-05-23 13:28:45 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+# install solr
+/usr/local/bin/brew install solr
+echo "Brewed solr"
+
+export SOLR_CONF=`brew --prefix solr`/libexec/example/solr/
+export SOLR_SCHEMA=`brew --prefix solr`/libexec/example/solr/conf/schema.xml
+
+alias solr=`solr $SOLR_CONF`
+
+echo
+echo "Add the following to your bash_profile or equivalent:"
+echo
+echo "export SOLR_CONF=\`brew --prefix solr\`/libexec/example/solr/"
+echo "export SOLR_SCHEMA=\`brew --prefix solr\`/libexec/example/solr/conf/schema.xml"
+echo
+echo "alias solr=\`solr \$SOLR_CONF\`"
+echo
+echo "-----------------------------------------------------------------"
+echo
+echo "Solr is now runnable with: solr"
+echo
+echo "Export your schema with: python manage.py build_schema -f \$SOLR_SCHEMA"
+echo
